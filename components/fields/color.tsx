88fcfb80dcd17b2e4bcfab8afbab3da94ec3bb8a
Hermes GitHub Specialist <hermes-agent@users.noreply.github.com>
2026-06-17 21:56:43 +0900

    fix: make TinaCMS 3 upgrade build on Node 22

--- components/fields/color.tsx
+++ components/fields/color.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { wrapFieldsWithMeta } from "tinacms";
 
 export const colorOptions = [
   "blue",
@@ -13,7 +12,7 @@ export const colorOptions = [
   "white",
 ];
 
-export const ColorPickerInput = wrapFieldsWithMeta(({ input }) => {
+export const ColorPickerInput = ({ input }) => {
   const inputClasses = {
     blue: "bg-blue-500 border-blue-600",
     teal: "bg-teal-500 border-teal-600",
@@ -49,4 +48,4 @@ export const ColorPickerInput = wrapFieldsWithMeta(({ input }) => {
       </div>
     </>
   );
-});
+};
--- components/fields/icon.tsx
+++ components/fields/icon.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { GoCircleSlash } from "react-icons/go";
-import { Button, wrapFieldsWithMeta } from "tinacms";
 import { Popover, Transition } from "@headlessui/react";
 import { Icon, IconOptions } from "../util/icon";
 import { BiChevronRight } from "react-icons/bi";
@@ -14,7 +13,7 @@ const parseIconName = (name: string) => {
   }
 };
 
-export const IconPickerInput = wrapFieldsWithMeta(({ input }) => {
+export const IconPickerInput = ({ input }) => {
   const [filter, setFilter] = React.useState("");
   const filteredBlocks = React.useMemo(() => {
     return Object.keys(IconOptions).filter((name) => {
@@ -34,11 +33,11 @@ export const IconPickerInput = wrapFieldsWithMeta(({ input }) => {
         {({ open }) => (
           <>
             <Popover.Button as={"span"}>
-              <Button
-                className={`text-sm h-11 px-4 ${InputIcon ? "h-11" : "h-10"}`}
-                size="custom"
-                rounded="full"
-                variant={open ? "secondary" : "white"}
+              <button
+                type="button"
+                className={`inline-flex items-center rounded-full border px-4 text-sm shadow-sm transition-colors ${
+                  InputIcon ? "h-11" : "h-10"
+                } ${open ? "bg-gray-100" : "bg-white"}`}
               >
                 {InputIcon && (
                   <InputIcon className="w-7 mr-1 h-auto fill-current text-blue-500" />
@@ -47,7 +46,7 @@ export const IconPickerInput = wrapFieldsWithMeta(({ input }) => {
                 {!InputIcon && (
                   <BiChevronRight className="w-5 h-auto fill-current opacity-70 ml-1" />
                 )}
-              </Button>
+              </button>
             </Popover.Button>
             <div className="absolute w-full min-w-[192px] max-w-2xl -bottom-2 left-0 translate-y-full">
               <Transition
@@ -128,4 +127,4 @@ export const IconPickerInput = wrapFieldsWithMeta(({ input }) => {
       </Popover>
     </div>
   );
-});
+};
